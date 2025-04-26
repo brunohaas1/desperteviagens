9417a96cfa6ed8791bd805da5138dd74fac1c8e4
Bruno <brunohaas3@hotmail.com>
2025-04-26 11:36:18 -0300

    Melhorias no formulário: máscara de telefone e validações

--- viagens/forms.py
+++ viagens/forms.py
@@ -5,3 +5,18 @@ class SolicitacaoOrcamentoForm(forms.ModelForm):
     class Meta:
         model = SolicitacaoOrcamento
         fields = ['nome', 'sobrenome', 'email', 'telefone', 'destino', 'data_viagem']
+
+        widgets = {
+            'telefone': forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'id_telefone',
+            'placeholder': '(00) 00000-0000',
+            'autocomplete': 'off',
+}),
+
+            
+            'data_viagem': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+            }),
+}
