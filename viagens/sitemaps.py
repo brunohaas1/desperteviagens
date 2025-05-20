d9c57c45bb42583a509317d435bda90c1fbc8b53
Bruno <brunohaas3@hotmail.com>
2025-05-20 10:21:38 -0300

    Adicionando a pagina produtos no sitemaps

--- viagens/sitemaps.py
+++ viagens/sitemaps.py
@@ -2,11 +2,11 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.8
-    changefreq = 'monthly'
+    priority = 1.0
+    changefreq = 'weekly'
 
     def items(self):
-        return ['home', 'cotacao', 'depoimentos', 'contato', 'sobre_nos', 'nossos_agentes']
+        return ['home', 'cotacao', 'depoimentos', 'contato', 'sobre_nos', 'nossos_agentes', 'produtos']
 
     def location(self, item):
         return reverse(item)
